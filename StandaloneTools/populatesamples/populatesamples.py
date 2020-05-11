5a2d9ad501ea9570cfd363e564a4248144f10e1e
theboginator <boginator55@gmail.com>
2020-05-11 12:04:45 -0400

    Add support framework for sample db population

--- StandaloneTools/populatesamples/populatesamples.py
+++ StandaloneTools/populatesamples/populatesamples.py
@@ -20,15 +20,15 @@ cur.execute("CREATE TABLE IF NOT EXISTS fpiweb_product (id INT AUTO_INCREMENT PR
 cur.execute("CREATE TABLE IF NOT EXISTS fpiweb_productcategory (id INT AUTO_INCREMENT PRIMARY KEY, prod_cat_name VARCHAR(30), prod_cat_descr longtext)")
 
 # Get JSON data
-file = open('templates/LocTier.json')
+file = open('/home/capstonetest/PycharmProjects/Food-Pantry-Inventory/StandaloneTools/populatesamples/templates/LocTier.json')
 loctier = json.load(file)
-file = open('templates/PalletBox.json')
+file = open('/home/capstonetest/PycharmProjects/Food-Pantry-Inventory/StandaloneTools/populatesamples/templates/PalletBox.json')
 palletbox = json.load(file)
-file = open('templates/Pallet.json')
+file = open('/home/capstonetest/PycharmProjects/Food-Pantry-Inventory/StandaloneTools/populatesamples/templates//Pallet.json')
 pallet = json.load(file)
-file = open('templates/Product.json')
+file = open('/home/capstonetest/PycharmProjects/Food-Pantry-Inventory/StandaloneTools/populatesamples/templates//Product.json')
 product = json.load(file)
-file = open('templates/ProductCategory.json')
+file = open('/home/capstonetest/PycharmProjects/Food-Pantry-Inventory/StandaloneTools/populatesamples/templates//ProductCategory.json')
 productcategory = json.load(file)
 
 for tier in loctier:
@@ -39,22 +39,6 @@ for tier in loctier:
     val = (str(loc_tier), str(loc_tier_descr))
     cur.execute(sql, val)
 
-for box in palletbox:
 
-    #"fields": {
-     # "pallet_id": 2,
-      #"box_id": 16,
-      #"box_number": "BOX12371",
-      #"product": 1,
-      #"exp_year": 2020,
-      #"exp_month_start": null,
-      #"exp_month_end": null,
-      #"box_status": "New"}
-    print(pallet['fields']['loc_tier'], (tier['fields']['loc_tier_descr']))
-    loc_tier = tier['fields']['loc_tier']
-    loc_tier_descr = tier['fields']['loc_tier_descr']
-    sql = "INSERT INTO fpiweb_loctier(loc_tier, loc_tier_descr) VALUES(%s, %s)"
-    val = (str(loc_tier), str(loc_tier_descr))
-    cur.execute(sql, val)
 
 db.close()
